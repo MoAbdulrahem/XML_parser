192ff336de0394ad815088a9a8dd47b8052fb5f9
MoAbdulrahem <1601227@eng.asu.edu.eg>
2021-07-12 01:39:57 +0200

    improved minify function

--- minify.py
+++ minify.py
@@ -1,12 +1,10 @@
 # THIS FILE SHOULD BE MODIFIED TO TAKE THE XML FILE INSTEAD OF THE DIRECTORY AFTER THE GUI DESIGN IS DONE
 
 #Minifying The XML File (by removing the new lines and some of the white spaces)
-def Minify(file_directory):
+def Minify(contents):
     '''
     Takes the XML file as an argument, and removes new lines and each two or more successive spaces
     '''
-    with open(file_directory,'r') as f:
-        contents = f.read()
     contents = contents.replace("   ","")
     contents = contents.replace("\n","")
     return contents
